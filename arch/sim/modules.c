05a026fa50ceda145ee6fd3f1c21bec098526faa
Hajime Tazaki <tazaki@nict.go.jp>
2013-01-08 00:43:42 +0900

    add gcov profile support for ns-3-dce.

--- arch/sim/modules.c
+++ arch/sim/modules.c
@@ -1,7 +1,6 @@
 #include "sim-assert.h"
 #include <linux/moduleparam.h>
 #include <linux/kmod.h>
-#include <linux/module.h>
 
 int modules_disabled = 0;
 char modprobe_path[KMOD_PATH_LEN] = "/sbin/modprobe";
@@ -12,40 +11,9 @@ static struct kernel_param g_empty_param_buffer;
 struct kernel_param *__start___param = &g_empty_param_buffer;
 struct kernel_param *__stop___param = &g_empty_param_buffer;
 
-const struct module_version_attribute no_modules[1] = {
-		{
-		.mattr	= {
-			.attr	= {
-				.name	= "version",
-				.mode	= S_IRUGO,
-			},
-			.show	= __modver_version_show,
-		},
-		.module_name	= KBUILD_MODNAME,
-		.version	= "V0",
-	}
-};
-
-const struct module_version_attribute **__start___modver = &no_modules;
-const struct module_version_attribute **__stop___modver  = &no_modules;
-
-static ssize_t store_uevent(struct module_attribute *mattr,
-			    struct module_kobject *mk,
-			    const char *buffer, size_t count)
-{
-	sim_assert (false);
-	return 0;
-}
-
-struct module_attribute module_uevent =
-	__ATTR(uevent, 0200, NULL, store_uevent);
-
 int __request_module(bool wait, const char *fmt, ...)
 {
   // we really should never be trying to load modules that way.
+  sim_assert (false);
   return 0;
 }
-
-struct ctl_table usermodehelper_table[] = {
-	{ }
-};

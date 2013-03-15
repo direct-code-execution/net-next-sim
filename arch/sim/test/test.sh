24eecf9cf86445cd972e481855af5aa1eaa49c5d
Hajime Tazaki <tazaki@nict.go.jp>
2013-03-15 23:29:58 +0900

    add sim test

---
+++ arch/sim/test/test.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+set -e
+#set -x
+# LD_LOG=symbol-fail
+VALGRIND=""
+FAULT_INJECTION=""
+
+if [ "$1" = "-g" ] ; then
+ VALGRIND="-g"
+elif [ "$1" = "-f" ] ; then
+ FAULT_INJECTION="-f"
+fi
+
+# FIXME
+export NS_ATTRIBUTE_DEFAULT='ns3::DceManagerHelper::LoaderFactory=ns3::DlmLoaderFactory[];ns3::TaskManager::FiberManagerType=UcontextFiberManager' 
+
+cd buildtop/source/dce
+LD_LIBRARY_PATH=${srctree} ./test.py -z test-runner-vdl -n ${VALGRIND} ${FAULT_INJECTION}

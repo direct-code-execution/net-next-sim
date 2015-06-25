1ffd1845d3b6b17015d17e5351cd716e31e733db
Hajime Tazaki <tazaki@sfc.wide.ad.jp>
2015-06-25 16:45:58 +0900

    add sanity checks to list_del operation
    this is a backport from the sim-ns3-dev-branch:
    793e2d6a3069aad93e40f362038fca0d67515fd7
    
    Signed-off-by: Hajime Tazaki <tazaki@sfc.wide.ad.jp>

--- arch/sim/workqueue.c
+++ arch/sim/workqueue.c
@@ -49,10 +49,12 @@ workqueue_function (void *context)
        {
          struct work_struct *work = list_first_entry(&wq->list,
                                                      struct work_struct, entry);
-         work_func_t f = work->func;
-         __list_del (work->entry.prev, work->entry.next);
-         work_clear_pending (work);
-         f(work);
+         if (work->entry.prev != LIST_POISON2) {
+           work_func_t f = work->func;
+           __list_del (work->entry.prev, work->entry.next);
+           work_clear_pending (work);
+           f(work);
+         }
        }
     }
 }
@@ -127,9 +129,11 @@ bool cancel_work_sync(struct work_struct *work)
   if (!list_empty(&work->entry))
     {
       // work was queued. now unqueued.
-      list_del_init(&work->entry);
-      work_clear_pending (work);
-      retval = 1;
+      if (work->entry.prev != LIST_POISON2) {
+        list_del_init(&work->entry);
+        work_clear_pending (work);
+        retval = 1;
+      }
     }
   return retval;
 }
